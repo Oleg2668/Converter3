73795ea1cd73d8fcc694975f4c85d92bebc41958
Oleg <olegpanin1968@gmail.com>
2024-06-09 20:49:10 +0300

    Initial commit

---
+++ src/app/Main.java
@@ -0,0 +1,16 @@
+package app;
+
+public class Main {
+    private static final double CONV_K = 1.60934;
+
+    public static void main(String[] args) {
+        double km = 1000;
+        double mls = 2000;
+
+        System.out.println("App for measures converting ");
+        System.out.println("Converter App.");
+        System.out.println("Version 2.0.");
+
+    }
+}
+
