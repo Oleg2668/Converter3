0e9b39529d78a35905d82c50a37ad62f2c295ecb
Oleg <olegpanin1968@gmail.com>
2024-06-09 22:02:02 +0300

    Add miles to kilometers  converter

--- src/app/Main.java
+++ src/app/Main.java
@@ -4,13 +4,26 @@ public class Main {
     private static final double CONV_K = 1.60934;
 
     public static void main(String[] args) {
-        double km = 1000;
+
         double mls = 2000;
 
         System.out.println("App for measures converting ");
         System.out.println("Converter App.");
         System.out.println("Version 2.0.");
+        double kilometers= convMlsToKilometers(mls);
+
+
+        System.out.println("Result is "  + kilometers + " km .");
 
     }
-}
+    private static double convMlsToKilometers(double mls) {
+        return mls * CONV_K;
+    }
+
+
+    }
+
+
+
+
 
